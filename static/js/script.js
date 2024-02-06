335e9e269d602ec00823a45fe64a8f01d36001b9
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-02-06 04:03:18 +0000

    Update quantity selector on product_detail.html and add JS to increment/decrement

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,21 @@ setTimeout(function () {
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 3000);
+
+// Increment quantity
+$('#increment').click(function(e) {
+   e.preventDefault();
+   var closestInput = $(this).closest('.input-group').find('.quantity')[0];
+   var currentValue = parseInt($(closestInput).val());
+   $(closestInput).val(currentValue + 1);
+});
+
+// Decrement quantity
+$('#decrement').click(function(e) {
+   e.preventDefault();
+   var closestInput = $(this).closest('.input-group').find('.quantity')[0];
+   var currentValue = parseInt($(closestInput).val());
+   $(closestInput).val(currentValue - 1);
+});
+
 });
