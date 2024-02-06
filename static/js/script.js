407e539c506d68108495a2ad586478c02b6b9b13
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-02-06 19:35:34 +0000

    Add ability to disable increment and decrement buttons on quantity selector in product_detail.html

--- static/js/script.js
+++ static/js/script.js
@@ -6,20 +6,38 @@ setTimeout(function () {
     alert.close();
 }, 3000);
 
-// Increment quantity
-$('#increment').click(function(e) {
+// increment and decrement quantity and prevent inputs of less than 1 and greater than 50
+let quantity = $('#quantity').val();
+if (quantity = 1){
+   $('#decrement').prop('disabled', true);
+} 
+$('#decrement').on('click', function(e) {
    e.preventDefault();
-   var closestInput = $(this).closest('.input-group').find('.quantity')[0];
-   var currentValue = parseInt($(closestInput).val());
-   $(closestInput).val(currentValue + 1);
-});
+   let currentValueNow = $('#quantity').val(); 
+   currentValueNow--;
+   $('#quantity').val(currentValueNow);
+      if (currentValueNow <= 1) {
+        $('#decrement').prop('disabled', true);
+      } else if (currentValueNow >= 50) {
+         $('#increment').prop('disabled', false);
+      }else {
+         $('#increment').prop('disabled', false);
+      }
+ });
 
-// Decrement quantity
-$('#decrement').click(function(e) {
+ $('#increment').on('click', function(e) {
    e.preventDefault();
-   var closestInput = $(this).closest('.input-group').find('.quantity')[0];
-   var currentValue = parseInt($(closestInput).val());
-   $(closestInput).val(currentValue - 1);
-});
+   let currentValueNow = $('#quantity').val();
+   currentValueNow++;
+   $('#quantity').val(currentValueNow);
+   $('#quantity').val(currentValueNow);
+   if (currentValueNow > 49) {
+     $('#increment').prop('disabled', true);
+   } else if (currentValueNow >= 1) {
+      $('#decrement').prop('disabled', false);
+   }else {
+      $('#increment').prop('disabled', false);
+   }
+ });
 
 });
