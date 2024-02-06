74cf62002b7b9564a199de43d211f39c6d9ad009
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-02-06 21:51:00 +0000

    Add add_to_bag view and context.py in bag app

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def view_bag(request):
     """
     A view to return bag page
     """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, product_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    print(request.POST)
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    
+    bag = request.session.get('bag', {})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] += quantity
+    else:
+        bag[product_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
