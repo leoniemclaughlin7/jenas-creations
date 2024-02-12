e7ac2c9a128e3630965dbc0a57918c37f35c094e
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-02-12 15:14:23 +0000

    Add checkout.html to checkout app

--- jenas_creations/settings.py
+++ jenas_creations/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 
 ]
 
@@ -67,6 +68,9 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'jenas_creations.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -81,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
