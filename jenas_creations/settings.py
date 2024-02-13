323c969965a3d6691fdf1af2287dd1f4540923fc
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-02-13 15:39:58 +0000

    Add stripe js, webhooks.py and webhook_handler.py

--- jenas_creations/settings.py
+++ jenas_creations/settings.py
@@ -191,3 +191,8 @@ CLOUDINARY_STORAGE = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
