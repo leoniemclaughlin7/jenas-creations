f3704d10ca0cb10acfb9e1be70d7808a8569d5dc
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-02-15 17:13:17 +0000

    Add Heroku url to allowed hosts in settings.py

--- jenas_creations/settings.py
+++ jenas_creations/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-leoniemclau-jenascreati-jdc4vvi3ret.ws-eu108.gitpod.io']
+ALLOWED_HOSTS = ['8000-leoniemclau-jenascreati-jdc4vvi3ret.ws-eu108.gitpod.io', 'jenas-creations-ad878282c6a4.herokuapp.com']
 
 
 # Application definition
