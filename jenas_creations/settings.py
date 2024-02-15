0a030491337dcb5c8c5b8367f41501f3e427b92a
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-02-15 22:46:47 +0000

    Add debug to True in settings.py

--- jenas_creations/settings.py
+++ jenas_creations/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-leoniemclau-jenascreati-jdc4vvi3ret.ws-eu108.gitpod.io', 'jenas-creations-ad878282c6a4.herokuapp.com']
 
