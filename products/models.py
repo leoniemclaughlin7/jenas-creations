332069805a9ad341688f6189cda223efe77e9471
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-02-23 17:23:05 +0000

    Add edit_product view, template and url to product app

--- products/models.py
+++ products/models.py
@@ -17,8 +17,8 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=False)
+    image = models.ImageField(null=True, blank=False)
 
     def __str__(self):
         return self.name
