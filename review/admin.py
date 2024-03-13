fe1d335746da79573fecbb6ada3f830e2aa7b774
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-03-13 23:05:20 +0000

    Add review admin in admin.py in review app

--- review/admin.py
+++ review/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Review
 
+
 admin.site.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """
+    Review admin set up, using list display
+     and list filter so admin
+    can filter the reviews by number of stars and date.
+    """
+    list_display = ('name', 'stars', 'created_on')
+    list_filter = ('stars', 'created_on')
