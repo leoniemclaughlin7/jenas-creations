4a6542b0791797cf70e6e070222066fb46a54b20
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-03-13 23:19:35 +0000

    Add CustomOrderAdmin to admin.py in the custom_order app

--- custom_order/admin.py
+++ custom_order/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import CustomOrder
 
-admin.site.register(CustomOrder)
+
+class CustomOrderAdmin(admin.ModelAdmin):
+    """
+    Custom order admin set up, using list display
+    so as admin panel will be ordered by the display
+    below and list filter so admin can easily find
+    orders.  
+    """
+    list_display = (
+        'material',
+        'category',
+        'price',
+        'created_on',
+    )
+    list_filter = ('material', 'category', 'created_on')
+
+admin.site.register(CustomOrder, CustomOrderAdmin)
 
 
