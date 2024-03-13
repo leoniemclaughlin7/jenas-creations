416fda3380239821d0a25f124e5f704d6ae11717
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-03-13 23:24:10 +0000

    Add ContactAdmin to admin.py in contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Contact admin set up, using list display
+    so as admin panel will be ordered by the display
+    below and list filter so admin can easily find
+    messages.  
+    """
+    list_display = (
+        'full_name',
+        'email',
+        'created_on',
+    )
+    list_filter = ('full_name', 'email', 'created_on')
+
+admin.site.register(Contact, ContactAdmin)
