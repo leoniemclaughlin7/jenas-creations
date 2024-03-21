63fbdba5d26a4090071621aafc0bf8d874e5d606
Leonie McLaughlin <leoniemclaughlin2@outlook.com>
2024-03-21 23:50:09 +0000

    Remove individual_total from add_product in views.py of product app

--- products/views.py
+++ products/views.py
@@ -103,7 +103,6 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            individual_total
             form.save()
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('product_detail', args=[product.id]))
